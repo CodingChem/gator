f28c531b90943440ee815d5073ceff5108100b10
Vegard Seines <vegsei@gmail.com>
2024-12-12 11:09:59 +0100

    Add: handleRegister function to support creating users

--- commands.go
+++ commands.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/codingchem/gator/internal/config"
+	"github.com/codingchem/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
@@ -35,6 +39,10 @@ func NewCommands() (commands, error) {
 		return commands{}, err
 	}
 	err = c.register("help", "\tDisplays a friendly help message\n\t`Usage: gator help`\n", handlerHelp)
+	if err != nil {
+		return commands{}, err
+	}
+	err = c.register("register", "\tRegister a new user\n\t`Usage: gator register $user`\n", handlerRegister)
 	if err != nil {
 		return commands{}, err
 	}
@@ -73,7 +81,28 @@ func handlerHelp(s *state, _ command) error {
 	fmt.Printf("Welcome to gator!\n\nUsage:\n")
 	for _, handler := range s.cmds.commandMap {
 		fmt.Println()
-		fmt.Printf("Command: %s\nDescription:\n%s", handler.name, handler.description)
+		fmt.Printf("Command: %s\nDescription:\n%s\n", handler.name, handler.description)
 	}
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Error: Invalid number of arguments!")
+	}
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err == nil {
+		return fmt.Errorf("Error: Username unavailable")
+	}
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserName:  cmd.args[0],
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Printf("User successfully created!\n\tusername: %s\n\tid: %v\n", user.UserName, user.ID)
 	return nil
 }
