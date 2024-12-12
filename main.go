b023648951c14fa5a040fe5dd8d877aec5f54b2f
Vegard Seines <vegsei@gmail.com>
2024-12-12 09:08:02 +0100

    Add: command, commands, state structs
    - Work with cli interface through reusable handlers

--- main.go
+++ main.go
@@ -3,24 +3,44 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/codingchem/gator/internal/config"
 )
 
 func main() {
+	globalState, err := initState()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Incorrect usage, consult 'help' for help")
+		os.Exit(1)
+	}
+	err = globalState.cmds.run(&globalState, command{
+		name: args[1],
+		args: args[2:],
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func initState() (state, error) {
 	globalConfig, err := config.Read()
 	if err != nil {
-		log.Panicf(err.Error())
+		return state{}, err
 	}
-	fmt.Printf("url: %s\nuser: %s\n", globalConfig.DB_CON_STRING, globalConfig.CurrentUser)
-	err = globalConfig.SetUser("vegard")
+	cmds, err := NewCommands()
 	if err != nil {
-		log.Panicf(err.Error())
+		return state{}, err
 	}
-	fmt.Println("Loading new user")
-	globalConfig, err = config.Read()
-	if err != nil {
-		log.Panicf(err.Error())
-	}
-	fmt.Printf("url: %s\nuser: %s\n", globalConfig.DB_CON_STRING, globalConfig.CurrentUser)
+	return state{
+		config: &globalConfig,
+		cmds:   &cmds,
+	}, nil
 }
